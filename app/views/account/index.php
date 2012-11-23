752df4ba83a111abb4f5e9742191ab24b953c783
ljxwjj <ljxwjj@163.com>
2012-11-23 15:17:48 +0800

    login demo

---
+++ app/views/account/index.php
@@ -0,0 +1,20 @@
+<form method="post" action="<?= site_url("account/login")?>">
+                <table border="0" cellpadding="0" cellspacing="0" width="42%">
+                    <tr>
+                        <td align="right">ÓÊ¼þµØÖ·</td>
+                        <td><input name="username" type="text" style="width:170px;" /></td>
+                    </tr>
+                    <tr>
+                        <td align="right">µÇÂ¼ÃÜÂë</td>
+                        <td><input name="password" type="password" style="width:170px;" /></td>
+                    </tr>
+                    <tr>
+                        <td colspan="2" align="center"><input name="submit" value="µÇÂ¼" type="submit"/></td>
+                    </tr>
+                    <tr>
+						<td align="right"><a href="reg.htm" style="color:#0167cc;">Á¢¿Ì×¢²á</a></td>
+						<td align="left"><a href="get_pwd.htm" style="color:#0167cc;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Íü¼ÇÃÜÂë?</a></td>
+                    </tr>
+                    
+                </table>
+</form>
